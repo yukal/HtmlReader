6b92ca9bbae531fe7a98fdb3ae53337163b418fb
Alexander Yukal <yukal@email.ua>
2022-01-24 13:13:48 +0200

    add tests

---
+++ test/test.mutable_state.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const HtmlReader = require('../src');
+const checkData = {};
+
+before(async () => {
+  const Fs = require('fs');
+  const Path = require('path');
+
+  const PATH_DATA = Path.join(process.cwd(), './test/data');
+  const htmlFilePath = Path.join(PATH_DATA, '1.html');
+
+  checkData.htmlContent = await Fs.promises.readFile(htmlFilePath, 'utf8');
+});
+
+describe('TODO: Mutable State', () => {
+  let parser;
+
+  before(() => {
+    parser = HtmlReader.from(checkData.htmlContent);
+  });
+
+  afterEach(() => {
+    parser.flushResults();
+  });
+
+  // TODO: all test cases
+  it.skip('tests', () => {});
+});
